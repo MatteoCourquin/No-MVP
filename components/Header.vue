a223d27aab2d612ee2e7967bb37f39b227e065d2
Matteo Courquin <matteo.courquin@gmail.com>
2021-05-20 12:58:55 +0200

    init Colors

--- components/Header.vue
+++ components/Header.vue
@@ -10,4 +10,10 @@
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+
+button{
+  background-color: var(--vs-primary);
+}
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -4,4 +4,6 @@
   </div>
 </template>
 
-<style></style>
+<style>
+@import '@/assets/css/_colors.css';
+</style>
